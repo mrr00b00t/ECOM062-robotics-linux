09dc6c51b4e07535a8d9a208d55dd5271248bceb
mrr00b00t <jass2@ic.ufal.br>
2022-06-09 23:47:11 -0300

    Adiciona outputs para os comandos da atv3.py.

--- atv3.py
+++ atv3.py
@@ -15,7 +15,7 @@ def ikine(qr):
     flag2 = (do - d_max) <= z <= (do - d_min)
     
     if not flag1 or not flag2:
-        return 'Posição não alcançável!'
+        print(f'Posição {qr} não alcançável!')
     
     d = do - z
     D = (x**2 + y**2 - l1**2 - l2**2) / (2*l1*l2)
@@ -23,7 +23,9 @@ def ikine(qr):
     theta2 = np.arctan2(np.sqrt(1 - D**2), D)
     theta1 = np.arctan2(y, x) - np.arctan2(l2*np.sin(theta2), (l1 + l2*np.cos(theta2)))
     
-    return [theta1, theta2, d, phi]
+    result = [theta1, theta2, d, phi]
+    
+    print(f'Parâmetros encontrados para cinemática reversa [theta1, theta2, d, phi] = {result}')
 
 qr1 = [0.2, 0.1, -0.015, np.pi/4]
 qr2 = [0.5, 0.1, -0.015, np.pi/4]
@@ -33,4 +35,4 @@ qrs = [qr1, qr2, qr3]
 
 print('Saidas da ikine([x, y, z, phi]) = [theta1, theta2, d, theta3]')
 for qr in qrs:
-    print(ikine(qr))
+    ikine(qr)
